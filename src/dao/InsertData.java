383149cc6db075c60e23f9385c4ac088cfd3f54d
Boaz E.C Theodoris <92237004+eddo4life@users.noreply.github.com>
2025-06-08 09:23:13 -0400

    Refactor InsertData class to improve variable declaration and enhance type handling

--- src/dao/InsertData.java
+++ src/dao/InsertData.java
@@ -16,7 +16,7 @@ public class InsertData extends AbstractTableModel {
      *
      */
     private static final long serialVersionUID = 1L;
-    String columnNames[];
+    String[] columnNames;
     ArrayList<InsertList> list;
     InsertList insertList = new InsertList();
     private Class<?>[] columnClass = getClas();
@@ -33,6 +33,7 @@ public class InsertData extends AbstractTableModel {
             }
 
 
+            assert arr != null;
             Class<?>[] columnC = new Class[arr.size()];
             int i = 0;
             for (String x : arr) {
@@ -41,31 +42,19 @@ public class InsertData extends AbstractTableModel {
                     case "number", "int", "integer", "unsigned smallint" -> columnC[i] = Integer.class;
                     case "decimal", "float", "double precision", "bigdatetime" -> columnC[i] = Double.class;
                     case "real" -> columnC[i] = Float.class;
-
                     case "numeric", "money", "smallmoney" -> columnC[i] = java.math.BigDecimal.class;
-
                     case "bit" -> columnC[i] = Boolean.class;
                     case "tinyint" -> columnC[i] = Byte.class;
                     case "smallint" -> columnC[i] = Short.class;
-
                     case "bigint", "unsigned int" -> columnC[i] = Long.class;
-
                     case "unsigned bigint" -> columnC[i] = java.math.BigInteger.class;
-
-                    case "binary", "java.sql.Date", "varbinary" -> columnC[i] = Byte[].class;
-
+                    case "binary", "java.sql.date", "varbinary" -> columnC[i] = Byte[].class;
                     case "image" -> columnC[i] = java.io.InputStream.class;
-
                     case "datetime", "smalldatetime", "bigtime" -> columnC[i] = java.sql.Timestamp.class;
-
                     // case "date"->{columnC[i]=java.sql.Time.class;}
                     case "date" -> columnC[i] = String.class;
-
                     case "time" -> columnC[i] = java.sql.Time.class;
-
-
                     default -> columnC[i] = String.class;
-
 
                 }
                 i++;
