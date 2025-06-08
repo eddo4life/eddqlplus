5b08f24e43a044a0bf97fcc8924a9be9d0a23c71
Boaz E.C Theodoris <92237004+eddo4life@users.noreply.github.com>
2025-06-08 00:13:35 -0400

    Refactor FileEvent class for improved readability and organization

--- src/view/ide/event/FileEvent.java
+++ src/view/ide/event/FileEvent.java
@@ -1,88 +1,85 @@
 package view.ide.event;
 
-import java.awt.Color;
+import controller.ide.filemanager.FileManager;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import view.Editor_section;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-import javax.swing.JOptionPane;
-
-import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
-
-import controller.ide.filemanager.FileManager;
-import view.Editor_section;
-
 public class FileEvent implements ActionListener, KeyListener {
 
-	RSyntaxTextArea editor_pane = Editor_section.textArea;
+    RSyntaxTextArea editor_pane = Editor_section.textArea;
 
-	public FileEvent() {
-		
+    public FileEvent() {
 
-	}
 
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
+    }
 
-		if (editor_pane.isEditable() && editor_pane.isEnabled()) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
-			String s = e.getActionCommand();
+        if (editor_pane.isEditable() && editor_pane.isEnabled()) {
 
-			if (s.equals("cut")) {
-				editor_pane.cut();
-			} else if (s.equals("copy")) {
-				editor_pane.copy();
-			} else if (s.equals("paste")) {
-				editor_pane.paste();
-			} else if (s.equals("select All")) {
-				editor_pane.selectAll();
-			} else if (s.equals("clear")) {
-				editor_pane.setText("");
-				// lineCountStuff();
-			} else if (s.equals("Save")) {
-				new FileManager().saveFile();
-			} else if (s.equals("Print")) {
-				Color color = editor_pane.getForeground();
-				try {
+            String s = e.getActionCommand();
 
-					if (color == Color.white) {
-						editor_pane.setForeground(Color.black);
-					}
-					editor_pane.print();
-				} catch (Exception evt) {
-					JOptionPane.showMessageDialog(null, evt.getMessage());
-				}
-				editor_pane.setForeground(color);
-			} else if (s.equals("Open")) {
-				new FileManager().openFile();
-			} else if (s.equals("New")) {
-				editor_pane.setEditable(true);
-				editor_pane.setText("");
-			} else if (s.equals("Close")) {
-				
-				Editor_section.close_font();
-			}
-		}
+            if (s.equals("cut")) {
+                editor_pane.cut();
+            } else if (s.equals("copy")) {
+                editor_pane.copy();
+            } else if (s.equals("paste")) {
+                editor_pane.paste();
+            } else if (s.equals("select All")) {
+                editor_pane.selectAll();
+            } else if (s.equals("clear")) {
+                editor_pane.setText("");
+            } else if (s.equals("Save")) {
+                new FileManager().saveFile();
+            } else if (s.equals("Print")) {
+                Color color = editor_pane.getForeground();
+                try {
 
-	}
+                    if (color == Color.white) {
+                        editor_pane.setForeground(Color.black);
+                    }
+                    editor_pane.print();
+                } catch (Exception evt) {
+                    JOptionPane.showMessageDialog(null, evt.getMessage());
+                }
+                editor_pane.setForeground(color);
+            } else if (s.equals("Open")) {
+                new FileManager().openFile();
+            } else if (s.equals("New")) {
+                editor_pane.setEditable(true);
+                editor_pane.setText("");
+            } else if (s.equals("Close")) {
+
+                Editor_section.close_font();
+            }
+        }
+
+    }
 
 }
