5765873bc056e8325a2d3e2a744a3a5e6bfdc145
Boaz E.C Theodoris <92237004+eddo4life@users.noreply.github.com>
2025-06-08 10:37:42 -0400

    Refactor CommandSystem class for improved readability and streamlined logic

--- src/controller/ide/command/system/CommandSystem.java
+++ src/controller/ide/command/system/CommandSystem.java
@@ -11,74 +11,67 @@ import view.Home;
 
 public class CommandSystem {
 
-	public CommandSystem(String cmd) {
+    public CommandSystem(String cmd) {
 
-		execute(cmd);
-	}
+        execute(cmd);
+    }
 
-	void execute(String cmd) {
-		if (!cls(cmd)) {
-			RSyntaxTextArea t = Editor_section.textArea;
-			switch (cmd) {
-			case "cls;" -> Editor_section.reset();
-			case "clear;" -> {
-				t.setText("");
-				//Editor.lineEditorPane.setText("");
-			}
-			case "exit;", "quit;", "close;" -> t.setEditable(false);//welcomepage...
-			case "hbar;", "hide bar" -> Home.toolBar.setVisible(false);
-			case "sbar;", "show bar" -> Home.toolBar.setVisible(true);
-			case "light;" -> {
+    void execute(String cmd) {
+        if (!cls(cmd)) {
+            RSyntaxTextArea t = Editor_section.textArea;
+            switch (cmd) {
+                case "cls;" -> Editor_section.reset();
+                case "clear;" -> {
+                    t.setText("");
+                    //Editor.lineEditorPane.setText("");
+                }
+                case "exit;", "quit;", "close;" -> t.setEditable(false);//welcome page...
+                case "hbar;", "hide bar" -> Home.toolBar.setVisible(false);
+                case "sbar;", "show bar" -> Home.toolBar.setVisible(true);
+                case "light;" -> {
 
-				//Editor_section.resetColor();
-			}
-			case "dark;" -> {
+                    //Editor_section.resetColor();
+                }
+                case "dark;" -> {
 
-				//Editor_section.resetColor();
-			}
-			case "open font;", "choose font;", "pick font;" -> Editor_section.westPanel_fontChooser_parent.setVisible(true);
-			case "font color;" -> {
-				Color color = JColorChooser.showDialog(Home.frame, "Choose a color", null);
-				t.setForeground(color);
-				Editor_section.editorpane_current_fgColor = color;
-			}
-			case "w -resize;"->{
-				boolean x= true;
+                    //Editor_section.resetColor();
+                }
+                case "open font;", "choose font;", "pick font;" -> Editor_section.westPanel_fontChooser_parent.setVisible(true);
+                case "font color;" -> {
+                    Color color = JColorChooser.showDialog(Home.frame, "Choose a color", null);
+                    t.setForeground(color);
+                    Editor_section.editorpane_current_fgColor = color;
+                }
+                case "w -resize;" -> {
+                    Home.frame.setResizable(!Home.frame.isResizable());
+                }
+                //case "create new table"->;
+                //cretate new strategies fro insert....
+                //case "insert into ...(to autmatically select the table..
+                //case "new insertion"...
+            }
+        }
 
-				if(Home.frame.isResizable()) {
-					Home.frame.setResizable(false);
-				}else {
-					Home.frame.setResizable(true);
-				}
-				
-			}
-			//case "create new table"->;
-			//cretate new strategies fro insert....
-			//case "insert into ...(to autmatically select the table..
-			//case "new insertion"...
-			}
-		}
+    }
 
-	}
-
-	boolean cls(String str) {
-		boolean cls = false;
-		if (str.length() > 4)
-			if (String.valueOf(str.charAt(str.length() - 5)).isBlank()) {
-				if (str.toLowerCase().substring(str.length() - 4, str.length()).equals("cls;")) {
-					Editor_section.reset();
-					cls = true;
-				}
-			} else if (str.length() == 4) {
-				Editor_section.reset();
-				cls = true;
-			}
-		return cls;
-	}
+    boolean cls(String str) {
+        boolean cls = false;
+        if (str.length() > 4)
+            if (String.valueOf(str.charAt(str.length() - 5)).isBlank()) {
+                if (str.toLowerCase().substring(str.length() - 4, str.length()).equals("cls;")) {
+                    Editor_section.reset();
+                    cls = true;
+                }
+            } else if (str.length() == 4) {
+                Editor_section.reset();
+                cls = true;
+            }
+        return cls;
+    }
 
 }
 
 /*
 
 
-*/
+ */
